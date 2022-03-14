6dca8cd41cc7a7066c37dcf885d449aa7ab7b988
TubiOb <tubiobaloluwa@gmail.com>
2022-03-14 12:56:47 -0700

    my first commit

--- 0x01-variables_if_else_while/.1-last_digit.c
+++ 0x01-variables_if_else_while/.1-last_digit.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <string.h>
 
-/* function main accepts the value of variable n and prints the last digit of the variable n. 
+/* function main accepts and prints the last digit of the variable n. 
  * It prints 4 strings which include:
  * "Last digit of" as the begining of the output,
  * "and is greater than 5" if the last digit of the given variable n is greater than 5,
@@ -22,15 +21,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (lastDigit = n % 10, lastDigit > 5)
 	{
-		printf("Last digit of %ld %s %d %s\n", n, "is", lastDigit, "and is greater than 5");
+		printf("Last digit of %ld %c %d %c\n", n, 'is', lastDigit, 'and is greater than 5');
 	}
-	else if (lastDigit = n % 10, lastDigit = 6 > n, lastDigit != 0)
+	else if (lastDigit = n % 10, lastDigit = 6 > n, lastDigit = (n != 0))
 	{
-		printf("Last digit of %ld %s %d %s\n", n, "is", lastDigit, "and is less than 6 and not 0");
+		printf("Last digit of %ld %c %d %c\n", n, 'is', lastDigit, 'and is less than 6 and not 0');
 	}
 	else
 	{
-		printf("Last digit of %ld %s %d %s\n", n, "is", lastDigit, "and is zero");
+		printf("Last digit of %ld %c %d %c\n", n, 'is', lastDigit, 'and is zero');
 	}
 	return (0);
 }
