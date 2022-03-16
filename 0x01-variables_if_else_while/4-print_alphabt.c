72b72a5c56a3cea96b53ba19a6f7fa43eb5de2c4
TubiOb <tubiobaloluwa@gmail.com>
2022-03-16 08:06:08 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/* 
+ * function main prints all the letters of the alphabetsbut skips q and e.
+ *
+ * Return: 0
+ *
+ */
+
+ int main(void)
+{
+	char c;
+	for (c='a'; c<='z'; c++)
+	{
+		if((c =='e') || (c =='q'))
+		{
+			continue;
+		}
+             else
+	     {
+		    putchar(c);
+	     }
+	}
+	putchar('\n');
+
+	return (0);
+}
