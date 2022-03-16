516222a2bd499a6db2e06c61e1802d9331979508
TubiOb <tubiobaloluwa@gmail.com>
2022-03-16 08:34:21 -0700

    M first commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,23 +4,23 @@
 
 /* 
  * function main prints all the letters of the alphabetsbut skips q and e.
- *
  * Return: 0
- *
+ **
  */
 
  int main(void)
 {
 	char c;
-	for (c='a'; c<='z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if((c =='e') || (c =='q'))
+		if((c == 'e') || (c == 'q'))
 		{
 			continue;
 		}
              else
 	     {
-		    putchar(c);
+		     putchar(c);
 	     }
 	}
 	putchar('\n');
