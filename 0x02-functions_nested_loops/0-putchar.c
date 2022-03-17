792e021b11407fb0ab9e6ae22a54074e504f19c5
TubiOb <tubiobaloluwa@gmail.com>
2022-03-16 21:06:29 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-putchar.c
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <main.h>
+
+/*
+ * function main prints _putchar followed by a new line
+ * Return 0
+*/
+
+int main(void)
+{
+	int i;
+
+
+	while (i = 1)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return 0
+}
